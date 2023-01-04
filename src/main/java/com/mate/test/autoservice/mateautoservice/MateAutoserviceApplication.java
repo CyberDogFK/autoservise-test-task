200d986d4c09dee3245fd5a25ee07637a94a6efb
AntonPav <streetreadger@gmail.com>
2023-01-04 13:26:27 +0200

    init commit

---
+++ src/main/java/com/mate/test/autoservice/mateautoservice/MateAutoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mate.test.autoservice.mateautoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateAutoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateAutoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mate/test/autoservice/mateautoservice/MateAutoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mate.test.autoservice.mateautoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateAutoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
